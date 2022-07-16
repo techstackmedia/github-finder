3f133388669e52db56db82409a0e7b9d33b79d59
techstackmedia <osagienoah04@gmail.com>
2022-07-16 18:00:44 +0100

    use of layout component to embed both Navbar and Footer components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router /*, Routes, Route*/ } from 'react-router-dom';
-import Navbar from './components/Layout/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
-import Footer from './components/Layout/Footer';
+import Footer from './components/Footer/Footer';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* <main className="container mx-auto px-3 pb-12">Content</main> */}
+          </Route>
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
