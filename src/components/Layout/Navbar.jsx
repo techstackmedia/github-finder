27406bf51358a375c04f23c87ded199733223ab9
techstackmedia <osagienoah04@gmail.com>
2022-07-16 16:16:53 +0100

    navbar styling

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,22 +1,23 @@
 import { Link } from 'react-router-dom';
 import { FaGithub } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import './Navbar.css';
 const Navbar = ({ title }) => {
   return (
-    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-      <div className="container mx-auto">
-        <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-3xl" />
-          <Link to="/" className="text-lg font-bold align-middle">
+    <nav className="navbar-main">
+      <div className="navbar-container">
+        <div className="navbar-nav-flex">
+          <FaGithub className="navbar-nav-github-finder-logo" />
+          <Link to="/" className="navbar-nav-github-finder-text">
             {title}
           </Link>
         </div>
-        <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+        <div className="navbar-nav-flex-items-equal">
+          <div className="navbar-nav-flex-items-ends">
+            <Link to="/" className="navbar-nav-bg-rounded">
               Home
             </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/about" className="navbar-nav-bg-rounded">
               About
             </Link>
           </div>
