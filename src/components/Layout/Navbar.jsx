259f71f4de032d089f128166ac3e98ad1852aced
techstackmedia <osagienoah04@gmail.com>
2022-07-16 16:29:05 +0100

    apply css module to all inline class selectors

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,23 +1,24 @@
 import { Link } from 'react-router-dom';
 import { FaGithub } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import './Navbar.css';
+import navbar from './Navbar.module.css';
+
 const Navbar = ({ title }) => {
   return (
-    <nav className="navbar-main">
-      <div className="navbar-container">
+    <nav className={navbar['navbar-main']}>
+      <div className={navbar['navbar-container']}>
         <div className="navbar-nav-flex">
-          <FaGithub className="navbar-nav-github-finder-logo" />
-          <Link to="/" className="navbar-nav-github-finder-text">
+          <FaGithub className={navbar['navbar-nav-github-finder-logo']} />
+          <Link to="/" className={navbar['navbar-nav-github-finder-text']}>
             {title}
           </Link>
         </div>
-        <div className="navbar-nav-flex-items-equal">
-          <div className="navbar-nav-flex-items-ends">
-            <Link to="/" className="navbar-nav-bg-rounded">
+        <div className={navbar['navbar-nav-flex-items-equal']}>
+          <div className={navbar['navbar-nav-flex-items-ends']}>
+            <Link to="/" className={navbar['navbar-nav-bg-rounded']}>
               Home
             </Link>
-            <Link to="/about" className="navbar-nav-bg-rounded">
+            <Link to="/about" className={navbar['navbar-nav-bg-rounded']}>
               About
             </Link>
           </div>
