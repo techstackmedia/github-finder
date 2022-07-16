017144869710dd6eb475848eaebc550d87233d90
techstackmedia <osagienoah04@gmail.com>
2022-07-16 16:45:03 +0100

    remove comments in public/index.html, comment out unused router and routes module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router /*, Routes, Route*/ } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 
 function App() {
